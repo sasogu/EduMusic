97af076584ba394bf05154ce138be68f1c6e7b1d
Blasco <sasogu@asdf.es>
2026-02-10 15:49:36 +0100

    152

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'v1.5.1';
+const SW_VERSION = 'v1.5.2';
 const CACHE = 'EduMúsic-' + SW_VERSION;
 const META_CACHE = 'EduMúsic-meta';
 const SCOPE_URL = new URL(self.registration.scope);
