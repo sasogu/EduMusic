128ad71674b72d0ca3720f0c3ef463179c1a4eb9
Blasco <sasogu@asdf.es>
2026-04-15 12:48:19 +0200

    educraft

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'v1.5.5';
+const SW_VERSION = 'v1.5.6';
 const CACHE = 'EduMúsic-' + SW_VERSION;
 const META_CACHE = 'EduMúsic-meta';
 const SCOPE_URL = new URL(self.registration.scope);
